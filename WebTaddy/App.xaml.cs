f8d6b60bc41e015c84aa6ce11304fbe93d290818
galacticgadgetry <galactic-gadgetry@gmail.com>
2026-02-02 08:52:00 -0500

    Add MVVM architecture and Main view

--- WebTaddy/App.xaml.cs
+++ WebTaddy/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using WebTaddy.ViewModels;
+using WebTaddy.Views;
 
 namespace WebTaddy
 {
@@ -9,6 +11,18 @@ namespace WebTaddy
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainViewModel mainViewModel = new();
+            MainWindow = new MainView()
+            {
+                DataContext = mainViewModel
+            };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 
 }
